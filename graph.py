329c8cab52ace871049091193bc5960c1efe87cb
Andrew Smith <erastos.gentry@gmail.com>
2020-07-08 04:39:45 -0500

    Added meeting functionality and vizs

--- graph.py
+++ graph.py
@@ -39,13 +39,15 @@ def get_snapshots(slice):
         yield slice[str(dates[i]):str(dates[i + 1])]
 
 
-def naive_merging(time_1, time_2, merge_threshold):
+def naive_merging(time_1, time_1_meetings, time_2, time_2_meetings, merge_threshold):
     """Merge graph connected components if the difference between the sets is below a threshold. Add all other components to a result set"""
     merged_groups = []
+    result_meeting = {}
     for i in range(len(time_1)):
         for j in range(len(time_2)):
             if len(time_1[i].symmetric_difference(time_2[j])) <= merge_threshold:
                 merged_groups.append(time_1[i].union(time_2[j]))
+                result_meeting[time_1[i].union(time_2[j])] = max(time_1_meetings[time_1[i]], time_2_meetings[time_2[j]])+1
                 break
     result = []
     for i in range(len(time_1)):
@@ -56,6 +58,7 @@ def naive_merging(time_1, time_2, merge_threshold):
                 break
         if not is_subset:
             result.append(time_1[i])
+            result_meeting[time_1[i]] = time_1_meetings[time_1[i]] + 1
 
     for j in range(len(time_2)):
         is_subset = False
@@ -65,8 +68,9 @@ def naive_merging(time_1, time_2, merge_threshold):
                 break
         if not is_subset:
             result.append(time_2[j])
+            result_meeting[time_2[j]] = time_2_meetings[time_2[j]] + 1
     result.extend(merged_groups)
-    return result
+    return result, result_meeting
 
 
 def graph_merge(comm, merge_threshold):
@@ -75,18 +79,25 @@ def graph_merge(comm, merge_threshold):
     t_1 = next(gs)
     g1 = create_graph(t_1)
     initial_groups = [group for group in nx.connected_components(g1) if len(group) > 2]
+    initial_groups = list(map(frozenset, initial_groups))
+    initial_groups_meeting = {group: 1 for group in initial_groups}
     prev_groups = initial_groups
+    prev_groups_meeting = initial_groups_meeting
     results = []
     for t_i in gs:
         g = create_graph(t_i)
         scc = [group for group in nx.connected_components(g) if len(group) > 2]
-        result_groups = naive_merging(prev_groups, scc, merge_threshold)
+        scc = list(map(frozenset, scc))
+        scc_meeting = {group: 1 for group in scc}
+        result_groups, result_group_meetings = naive_merging(prev_groups, prev_groups_meeting, scc, scc_meeting, merge_threshold)
         prev_groups = result_groups
-        results.append(result_groups)
+        prev_groups_meeting = result_group_meetings
+        results.append((result_groups, result_group_meetings))
     return results[-1]
 
 
 if __name__ == '__main__':
     NUM_RECORDS = 200000
     network, basic, comm = open_files(NUM_RECORDS)
-    graph_merge(comm, 3)
+    result = graph_merge(comm, 3)
+    print(result)
--- visual.py
+++ visual.py
@@ -19,6 +19,7 @@ def get_mthresh_data(merge_threshold):
 
 def get_group_numbers(merge_threshold):
     mthresh_data = get_mthresh_data(merge_threshold)
+    mthresh_data = list(map(lambda x: x[0], mthresh_data))
     num_lengths = map(len, mthresh_data)
 
     plt.plot(list(range(1, merge_threshold + 1)), list(num_lengths))
@@ -30,10 +31,11 @@ def get_group_numbers(merge_threshold):
 
 def get_group_average(merge_threshold):
     mthresh_data = get_mthresh_data(merge_threshold)
+    mthresh_data = list(map(lambda x: x[0], mthresh_data))
     averages = []
     for i in mthresh_data:
         group_lengths = list(map(len, i))
-        averages.append(sum(group_lengths)//len(group_lengths))
+        averages.append(sum(group_lengths) // len(group_lengths))
 
     plt.plot(list(range(1, merge_threshold + 1)), averages)
     plt.xlabel("Merge Threshold")
@@ -42,6 +44,19 @@ def get_group_average(merge_threshold):
     plt.show()
 
 
-# get_group_numbers(10)
-get_group_average(10)
+def get_meetings_per_group(merge_threshold):
+    mthresh_data = get_mthresh_data(merge_threshold)
+    meetings_per_group = []
+    for i in mthresh_data:
+        total_meetings = sum(i[1].values())
+        meetings_per_group.append(total_meetings / len(i[0]))
+    plt.plot(list(range(1, merge_threshold + 1)), meetings_per_group)
+    plt.xlabel("Merge Threshold")
+    plt.ylabel("Meetings per Group")
+    plt.title("Meetings per Group vs Merge Threshold (n={})".format(num_records))
+    plt.show()
 
+
+# get_group_numbers(10)
+# get_group_average(10)
+get_meetings_per_group(10)
