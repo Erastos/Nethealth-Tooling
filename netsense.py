e49e06bd00fa6d624d64ed4b572c82815a9e9995
Andrew Smith <erastos.gentry@gmail.com>
2020-10-03 16:04:47 -0400

    Initial Commit

---
+++ netsense.py
@@ -0,0 +1,11 @@
+import enum
+
+files = {
+    "Comm": "./NetHealth Data/telcodata.txt",
+    "Basic": "./NetHealth Data/BasicSurvey(3-6-20).csv",
+    "Network": "./NetHealth Data/NetWorkSurvey(2-28-20).csv"
+}
+
+
+class Headers(enum):
+    DATE_TIME = 0
